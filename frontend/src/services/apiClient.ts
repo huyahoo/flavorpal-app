1b20d5241c238ace2a04e469398f21c0f7682c9f
HUY <takasugi.tut@gmail.com>
2025-05-28 19:44:55 +0900

    test deployment

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/auth';
 
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_BASE_URL = 'http://54.238.179.149:8000';
+const API_BASE_URL = 'https://54.238.179.149:8000';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
